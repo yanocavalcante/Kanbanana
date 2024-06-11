ffc03750529aa53f9f414dc160b658d04def7d1e
Layon <layonjroliveira@gmail.com>
2024-06-11 11:10:31 -0300

    Implementado váriaveis de ambiente

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import connectDataBase from './src/database/db.js';
 import userRoute from './src/routes/user.route.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 //Porta que o servidor disponibiliza OU a porta 3000
 
 connectDataBase();
 app.use(express.json()); // mandar dados json para nossa api
--- src/database/db.js
+++ src/database/db.js
@@ -4,11 +4,11 @@ import mongoose from 'mongoose';
 const connectDataBase = () => {
     console.log("Estabelecendo conexão com o banco de dados...");
 
-    mongoose.connect("mongodb+srv://layonjroliveira:hBhVTVTi8DBMAOrD@cluster0.xwmiktn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    mongoose.connect(process.env.MONGODB_URI ,
         { useNewUrlParser: true, useUnifiedTopology: true }
     )
-    .then( ()=> console.log("MongoDB Atlas conectado!"))
-    .catch( (error) => console.log("Erro ao conectar com o MongoDB Atlas: " + error));
+        .then(() => console.log("MongoDB Atlas conectado!"))
+        .catch((error) => console.log("Erro ao conectar com o MongoDB Atlas: " + error));
 }
 
 export default connectDataBase;
