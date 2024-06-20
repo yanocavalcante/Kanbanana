b33c2dcc20f10cb93dafe4aa51e678dabc3af5b2
matheussteinbach <matheussteimbach4@gmail.com>
2024-06-20 12:01:04 -0300

    feat: adiciona components Login e Register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Register from "./components/Register"
+import Login from "./components/Login"
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Login />
     </>
   )
 }
