d36388fd50400a62ebf8adb185db84927778af02
matheussteinbach <matheussteimbach4@gmail.com>
2024-06-23 15:54:50 -0300

    feat: adiciona verificacao dos campos auth, padronizacao input e mensagem de erro

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,15 +1,47 @@
 import React, { useState } from 'react';
 import './AuthStyled.css';
+import { Input } from '../../components/Input/Input'
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { signinSchema } from '../../schemas/signinSchema';
+import { signupSchema } from '../../schemas/signupSchema';
+import { ErrorSpan } from './ErrorSpanStyled';
 
 const Auth = () => {
+  const {
+    register: registerSignup, 
+    handleSubmit: handleSubmitSignup,
+    reset: resetSignup,
+    formState: {errors: errorsSignup}
+  } = useForm({resolver: zodResolver(signupSchema)})
+  
+  const {
+    register: registerSignin, 
+    handleSubmit: handleSubmitSignin,
+    reset: resetSignin,
+    formState: {errors: errorsSignin}
+  } = useForm({resolver: zodResolver(signinSchema)})
+  const navigate = useNavigate()
+
+  function signinHandleSubmit(data){
+    console.log(data)
+  }
+
+  function signupHandleSubmit(data){
+    console.log(data)
+  }
+
   const [isRegistro, setIsRegistro] = useState(true);
 
   const handleRegistroClick = () => {
     setIsRegistro(true);
+    resetSignup()
   };
 
   const handleLoginClick = () => {
     setIsRegistro(false);
+    resetSignin()
   };
 
   return (
@@ -31,28 +63,34 @@ const Auth = () => {
             Login
           </button>
         </div>
-        <form id="form-content">
+        <form id="form-content" onSubmit={isRegistro ?  handleSubmitSignup(signupHandleSubmit): handleSubmitSignin(signinHandleSubmit)}>
           {isRegistro ? (
             <>
-              <input type="text" name="nome" placeholder="Nome" />
-              <input type="email" name="email" placeholder="E-mail" />
-              <input type="password" name="senha" placeholder="Senha" />
-              <input type="password" name="confirmacao_senha" placeholder="Confirmação de senha" />
+              <Input type="text" name="name" placeholder="Nome" register={registerSignup}/>
+              {errorsSignup.name && <ErrorSpan> {errorsSignup.name.message} </ ErrorSpan>}
+              <Input type="email" name="email" placeholder="E-mail" register={registerSignup}/>
+              {errorsSignup.email && (<ErrorSpan> {errorsSignup.email.message} </ ErrorSpan>)}
+              <Input type="password" name="password" placeholder="Senha" register={registerSignup}/>
+              {errorsSignup.password && <ErrorSpan> {errorsSignup.password.message} </ ErrorSpan>}
+              <Input type="password" name="confirmPassword" placeholder="Confirmação de senha" register={registerSignup}/>
+              {errorsSignup.confirmPassword && <ErrorSpan> {errorsSignup.confirmPassword.message} </ ErrorSpan>}
             </>
           ) : (
             <>
-              <input type="email" name="email" placeholder="E-mail" />
-              <input type="password" name="senha" placeholder="Senha" />
+              <Input type="email" name="email" placeholder="E-mail" register={registerSignin}/>
+              {errorsSignin.email && (<ErrorSpan> {errorsSignin.email.message} </ ErrorSpan>)}
+              <Input type="password" name="password" placeholder="Senha" register={registerSignin}/>
+              {errorsSignin.password && <ErrorSpan> {errorsSignin.password.message} </ ErrorSpan>}
             </>
           )}
+          <div className="entrar-checkbox-container">
+            <label>
+              <input type="checkbox" name={isRegistro ? 'termos' : 'manter_conectado'} />
+              {isRegistro ? 'Confirmo que aceito os termos de serviço' : 'Manter-me conectado'}
+            </label>
+            <button className="entrar-container" type="submit">Entrar</button>
+          </div>
         </form>
-        <div className="entrar-checkbox-container">
-          <label>
-            <input type="checkbox" name={isRegistro ? 'termos' : 'manter_conectado'} />
-            {isRegistro ? 'Confirmo que aceito os termos de serviço' : 'Manter-me conectado'}
-          </label>
-          <button className="entrar-container" type="submit">Entrar</button>
-        </div>
       </div>
     </div>
   );
