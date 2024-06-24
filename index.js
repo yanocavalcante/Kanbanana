81711caaf9d68403b9d7ac8e45f4df24521f6b96
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-06-23 21:34:19 -0300

    Formatação

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-import express from 'express';
-import connectDataBase from './src/database/db.js';
-import dotenv from 'dotenv';
+import express from "express";
+import connectDataBase from "./src/database/db.js";
+import dotenv from "dotenv";
 
-import userRoute from './src/routes/user.route.js';
-import authRoute from './src/routes/auth.route.js';
-
+import userRoute from "./src/routes/user.route.js";
+import authRoute from "./src/routes/auth.route.js";
+import boardRoute from "./src/routes/board.route.js";
 
 dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3000 //Porta que o servidor disponibiliza OU a porta 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 connectDataBase();
-app.use(express.json()); // mandar dados json para nossa api
+app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
+app.use("/board", boardRoute);
 
 
-app.listen(port, () => console.log(`Servidor Rodando na porta ${port}`))
+app.listen(port, () => console.log(`Servidor Rodando na porta ${port}`));
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,31 @@
-import userService from '../services/user.service.js';
-
+import {
+    createService,
+    findAllService,
+    findByIdService,
+    updateService,
+} from "../services/user.service.js";
 
 const create = async (req, res) => {
     try {
-        const { name, username, email, password, avatar, background } = req.body;
-        if (!name || !username || !email || !password || !avatar || !background) {
-            res.status(400).json({ "message": "Submit all fields for resgistration" })
+        const { name, username, email, password, avatar, background } =
+            req.body;
+        if (
+            !name ||
+            !username ||
+            !email ||
+            !password ||
+            !avatar ||
+            !background
+        ) {
+            res.status(400).json({
+                message: "Submit all fields for resgistration",
+            });
         }
 
-        const user = await userService.createService(req.body);
+        const user = await createService(req.body);
 
         if (!user) {
-            return res.status(400).json({ "message": "Error to create user" })
+            return res.status(400).json({ message: "Error to create user" });
         }
 
         res.status(201).json({
@@ -22,58 +36,73 @@ const create = async (req, res) => {
                 username,
                 email,
                 avatar,
-                background
+                background,
             },
-        })
+        });
     } catch (err) {
-        res.status(500).send({ message: err.message })
+        res.status(500).send({ message: err.message });
     }
 };
 
 const findAll = async (req, res) => {
     try {
-        const users = await userService.findAllService();
+        const users = await findAllService();
 
         if (!users) {
-            return res.status(404).json({ "message": "Users not found" })
+            return res.status(404).json({ message: "Users not found" });
         }
 
-        res.status(200).json(users)
+        res.status(200).json(users);
     } catch (err) {
-        res.status(500).send({ message: err.message })
+        res.status(500).send({ message: err.message });
     }
-}
+};
 
-    const findById = async (req, res) => {
-        const user = req.user
-        res.status(200).json(user)
+const findById = async (req, res) => {
+    try {
+        const user = req.user;
+
+        res.send(user);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
+};
 
-    const update = async (req, res) => {
-        try {
-            const { name, username, email, password, avatar, background } = req.body;
+const update = async (req, res) => {
+    try {
+        const { name, username, email, password, avatar, background } =
+            req.body;
 
-            if (!name && !username && !email && !password && !avatar && !background) {
-                res.status(400).json({ "message": "Submit at least one fields for update" })
-            }
-
-            const { id, user } = req;
-
-            // Atualizando o usuário nessas linhas. Se o campo não for enviado, ele não será atualizado.
-            await userService.updateService(
-                id,
-                name,
-                username,
-                email,
-                password,
-                avatar,
-                background
-            );
-
-            res.status(200).json({ "message": "User updated successfully!" })
-        } catch (err) {
-            res.status(500).send({ message: err.message })
+        if (
+            !name &&
+            !username &&
+            !email &&
+            !password &&
+            !avatar &&
+            !background
+        ) {
+            res.status(400).json({
+                message: "Submit at least one fields for update",
+            });
         }
+
+        const { id, user } = req;
+
+        // Atualizando o usuário nessas linhas. Se o campo não for enviado, ele não será atualizado.
+        await updateService(
+            id,
+            name,
+            username,
+            email,
+            password,
+            avatar,
+            background
+        );
+
+        res.status(200).json({ message: "User updated successfully!" });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
+};
 
 export default { create, findAll, findById, update };
