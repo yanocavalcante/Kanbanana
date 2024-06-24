0f3a03f97943acf4f3d45c2d760f4e2778c12c68
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-06-23 21:34:04 -0300

    Update do Model de Board

---
+++ src/models/Board.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const BoardSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    lists: {
+        type: Array,
+        required: true,
+    },
+});
+
+const Board = mongoose.model("Board", BoardSchema);
+
+
+export default Board;
