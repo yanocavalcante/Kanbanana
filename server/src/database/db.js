e75bdcd4a0ffde6a90acd316abdc17652a6a354b
Matheus <matheussteinbach11@gmail.com>
2024-06-24 20:54:27 -0300

    fix: direcionamento collection db

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectDatabase = () => {
   console.log("Waiting connection to database...");
   mongoose
-    .connect(
-      process.env.MONGODB_URI
+    .connect( process.env.MONGODB_URI,
+      { dbName: process.env.MONGODB_DATABASE }
     )
     .then(() => console.log("MongoDB Atlas Connected"))
     .catch((error) => console.log(error));
