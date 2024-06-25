71dbb37e136973af41d0993c0271023c0e752ea3
Matheus <matheussteinbach11@gmail.com>
2024-06-25 20:51:02 -0300

    feat: adiciona button Editar Perfil

--- client/src/pages/Profile/ProfileStyled.jsx
+++ client/src/pages/Profile/ProfileStyled.jsx
@@ -1,5 +1,14 @@
 import styled from 'styled-components'
 
+export const ProfilePage = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    max-width: 80%;
+    margin: 0 auto;
+`
+
 export const ProfileContainer = styled.div`
     width: 100%;
     display: flex;
@@ -20,4 +29,13 @@ export const ProfileContainer = styled.div`
     h1{
         padding: 1rem;
     }
+`
+
+export const Button = styled.button`
+    display: flex;
+    width: 10rem;
+    height: 3rem;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
 `
