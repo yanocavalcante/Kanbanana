d03a934ee990d3ccbdd92e385f3288f3e83887b3
Matheus <matheussteinbach11@gmail.com>
2024-06-26 16:06:13 -0300

    feat: adiciona campos de edição no perfil

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,6 +1,11 @@
 import { useContext } from "react"
 import { UserContext } from "../../Context/UserContext"
-import { ProfilePage, ProfileContainer, Button } from './ProfileStyled'
+import { ProfilePage, ProfileContainer, Button, EditContainer } from './ProfileStyled'
+import { EditInput } from '../../components/EditInput/EditInput'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { editSchema } from '../../schemas/editSchema';
+//import { editUser } from '../../services/userServices';
 
 export default function Profile() {
 
@@ -10,10 +15,28 @@ export default function Profile() {
         _id: "667a0691ce5f2ba2754e215f",
         name: "Matheus Steinbach",
         username: "theus",
+        email: 'matheus@gmail.com',
         password: '123456',
         avatar: 'https://pbs.twimg.com/media/GQe9lMYW8AAO-vL?format=jpg&name=900x900'
     }
 
+    const {
+        register, 
+        handleSubmit,
+        reset,
+        formState: {errors}
+      } = useForm({resolver: zodResolver(editSchema)})
+
+/*      async function editHandleSubmit(data){
+        try {
+          const response = await editUser(data, id)
+          Cookies.set("token", response.data.token, { expires: 1 }) // Token não definido 
+          navigate('/home')
+        } catch (error){
+          console.log(error)
+        }
+      }
+*/
     return (
         <ProfilePage>
             <ProfileContainer>
@@ -21,7 +44,21 @@ export default function Profile() {
                 <h1> {user.name} </h1>
                 <h2> @{user.username}</h2>
             </ProfileContainer>
-            <Button> Editar Perfil </Button>
+            <EditContainer>
+                <h1> Editar Perfil </h1>
+                <form /*onSubmit={handleSubmit(editHandleSubmit)}*/>
+                    <label>Nome</label>
+                    <EditInput type="text" name="name" value={user.name} register={register}/>
+                    <label>Usuário</label>
+                    <EditInput type="text" name="username" value={user.username} register={register}/>
+                    <label>E-mail</label>
+                    <EditInput type="email" name="email" value={user.email} register={register}/>
+                    <label>Senha</label>
+                    <EditInput type="password" name="password" value={user.password} register={register}/>
+                    <label>Confirme sua nova senha</label>
+                    <EditInput type="password" name="confirmPassword" value={user.password} register={register}/>
+                </form>
+            </EditContainer>
         </ProfilePage>
     )
 }
--- client/src/pages/Profile/ProfileStyled.jsx
+++ client/src/pages/Profile/ProfileStyled.jsx
@@ -2,21 +2,21 @@ import styled from 'styled-components'
 
 export const ProfilePage = styled.div`
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     align-items: center;
     justify-content: center;
-    max-width: 80%;
+    width: 80%;
     margin: 0 auto;
+    padding-top: 4rem;
+    gap: 10rem;
 `
 
 export const ProfileContainer = styled.div`
-    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     min-height: 60vh;
-    padding-top: 4rem;
 
     img {
         width: 250px;
@@ -24,6 +24,8 @@ export const ProfileContainer = styled.div`
         border-radius: 50%;
         object-fit: cover;
         object-position: center;
+        margin-top: 2rem;
+        margin-bottom: 1rem;
     }
 
     h1{
@@ -38,4 +40,15 @@ export const Button = styled.button`
     justify-content: center;
     align-items: center;
     font-size: 1.2rem;
+`
+
+export const EditContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 25%;
+
+    form {
+        align-items: flex-start;
+        padding-top: 1rem;
+    }
 `
