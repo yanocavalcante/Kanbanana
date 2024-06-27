35c7a8bbe9ba251763523e74075b8acbe020b2e5
Matheus <matheussteinbach11@gmail.com>
2024-06-27 18:52:18 -0300

    fix: estado padrao da autenticacao

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -1,9 +1,10 @@
 import React, { createContext, useContext, useState } from 'react'
+import Cookies from 'js-cookie'
 
 const AuthContext = createContext()
 
 export default function AuthProvider ({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(Cookies.get("token"))
 
   const login = () => setIsAuthenticated(true)
   const logout = () => setIsAuthenticated(false)
