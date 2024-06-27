56b7cc572cfd3167dcfcae47fd013c2b5600d8dd
Ale <ale.benedetp@gmail.com>
2024-06-27 18:18:17 -0300

    Save my local changes before applying stash

--- server/src/routes/board.route.js
+++ server/src/routes/board.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { create, findAll } from "../controllers/board.controller.js";
+import { create, findAll, findById, update } from "../controllers/board.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/", authMiddleware, create);
 router.get("/", findAll);
+router.get("/:id", authMiddleware, findById);
+router.put("/:id", authMiddleware, update);
 
 export default router;
