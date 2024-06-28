87758a0cd0b1d0e889183bf1181ee54f24990fcd
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-06-28 20:55:00 -0300

    refact: refatorar serviço de login

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,7 +1,22 @@
-import User from '../models/User.js';
+import bcrypt from "bcrypt";
+import "dotenv/config";
+import userRepositories from "../repositories/user.repositories.js";
 import jwt from 'jsonwebtoken';
 
-const loginService = (email) => User.findOne({ email: email }).select("+ password");
+const loginService = async (email, password) => {
+    const user = await userRepositories.findByEmailUserRepository(email);
+
+    if (!user) throw new Error("Wrong password or username");
+
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) throw new Error("Invalid password");
+  
+    const token = generateToken(user.id);
+  
+    return token;
+}
 
 const generateToken = (id) => jwt.sign({ id: id }, process.env.SECRET_JWT, { expiresIn: 86400 })
 
