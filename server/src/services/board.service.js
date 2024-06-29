60ad18b655249577228c2d3c3deb9293eff650ac
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-06-28 21:48:44 -0300

    refact: refatorando serviços de Board

--- server/src/services/board.service.js
+++ server/src/services/board.service.js
@@ -1,15 +1,93 @@
-import Board from "../models/Board.js";
+import boardRepositories from "../repositories/board.repositories.js";
 
-const createService = (body) => Board.create(body);
+const createBoardService = async ({ name }, userId) => {
+    const name = name;
+    if (!name) throw new Error("Submit all fields for registration");
 
-const findAllService = (offset, limit) =>
-    Board.find().sort({ _id: -1 }).skip(offset).limit(limit);
+    const { id } = await boardRepositories.createBoardRepository(name, userId);
 
-const countBoards = () => Board.countDocuments();
+    return {
+        message: "Board created successfully!",
+        board: { id, name },
+    };
+};
 
-const findByIdService = (id) => Board.findById(id);
+const findAllService = async (offset, limit) => {
+    limit = Number(limit);
+    offset = Number(offset);
 
-const updateService = (id, body) => Board.findByIdAndUpdate(id, body, { new: true });
+    if (!limit) {
+        limit = 5;
+    }
 
-export { createService, findAllService, countBoards, findByIdService, updateService};
+    if (!offset) {
+        offset = 0;
+    }
 
+    const boards = await boardRepositories.findAllBoardRepository(
+        offset,
+        limit
+    );
+
+    const total = await boardRepositories.countBoardRepository();
+
+    const next = offset + limit;
+    const nextUrl =
+        next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+        previous != null
+            ? `${currentUrl}?limit=${limit}&offset=${previous}`
+            : null;
+
+    boards.shift();
+
+    return {
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+
+        results: boards.map((board) => ({
+            id: board._id,
+            name: board.name,
+            username: board.user.username,
+            avatar: board.user.avatar,
+        })),
+    };
+};
+
+const findByIdService = async (id) => {
+    const board = await boardRepositories.findBoardByIdRepository(id);
+
+    if (!board) throw new Error("Board not found");
+
+    return {
+        id: board._id,
+        name: board.name,
+        username: board.user.username,
+        avatar: board.user.avatar,
+    };
+};
+
+const updateService = async (id, name) => {
+    if (!name) throw new Error("Submit at least one field to update the board");
+
+    const board = await boardRepositories.findBoardByIdRepository(id);
+
+    if (!board) throw new Error("Board not found");
+
+    if (board.user._id != userId)
+        throw new Error("You didn't create this Board");
+
+    await boardRepositories.updateBoardRepository(id, name);
+};
+
+export default {
+    createBoardService,
+    findAllService,
+    findByIdService,
+    updateService,
+};
