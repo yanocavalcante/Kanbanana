8dd2c6f9d90028199b7abce051166d16e2d543a6
Matheus <matheussteinbach11@gmail.com>
2024-06-29 03:01:30 -0300

    refactor: mudanca de rotas para user e board

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -11,29 +11,28 @@ export function signup(data) {
         username: generateUsername(data.name),
         avatar: "https://upload.wikimedia.org/wikipedia/pt/8/86/Avatar_Aang.png",
     };
-    const response = axios.post(`${baseURL}/user/`, body);
+    const response = axios.post(`${baseURL}/user/create`, body);
     return response;
 }
 
 export function signin(data) {
-    const response = axios.post(`${baseURL}/auth/`, data);
+    const response = axios.post(`${baseURL}/auth/login`, data);
     return response;
 }
 
 export async function userLogged() {
-    const response = await axios.get(`${baseURL}/user/`, {
+    const response = await axios.get(`${baseURL}/user/findById`, {
         headers: {
             Authorization: `Bearer ${Cookies.get("token")}`
         }
     })
-    return response.data[0]
+    return response.data
 }
 
-export function editUser(body) {
-    const {user} = useUser()
+export function editUser(body, id) {
 
     delete body.confirmPassword
-    const response = axios.patch(`${baseURL}/user/${user.id}`, body, {
+    const response = axios.patch(`${baseURL}/user/update/${id}`, body, {
         headers: {
             Authorization: `Bearer ${Cookies.get("token")}`,
         }
