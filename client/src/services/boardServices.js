96a80dfd0721b9cbb73fa34266d8adf37bc94fe6
Matheus <matheussteinbach11@gmail.com>
2024-07-01 12:55:29 -0300

    fix: rodar no deploy

--- client/src/services/boardServices.js
+++ client/src/services/boardServices.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import Cookies from "js-cookie"
 
-const baseURL = "http://localhost:3000";
+const baseURL = "https://kanbanana-54dp.onrender.com/";
 
 export async function getBoardById(id) {
     const response = await axios.get(`${baseURL}/board/${id}`)
--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from 'js-cookie'
 
-const baseURL = "http://localhost:3000";
+const baseURL = "https://kanbanana-54dp.onrender.com/";
 
 export function signup(data) {
     delete data.confirmPassword
